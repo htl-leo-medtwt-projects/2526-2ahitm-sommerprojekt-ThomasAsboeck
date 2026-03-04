7596efa9eb5e135f6849831d9b3da5bdba822312
Thomas <t.asboeck@students.htl-leonding.ac.at>
2026-03-04 13:51:08 +0100

    Refactor game loop initialization to use requestAnimationFrame for smoother rendering

--- project/movementPrototypeV1/js/GameUpdate.js
+++ project/movementPrototypeV1/js/GameUpdate.js
@@ -4,5 +4,17 @@ let VARIABLES = {
 }
 
 function startGame() {
-    gameLoop();
+    requestAnimationFrame(gameLoop);
+}
+
+let lastTime = 0;
+
+function gameLoop(currentTime) {
+    const dt = (currentTime - lastTime) / 1000;  // ms → seconds
+    lastTime = currentTime;
+
+    //const [newVelX, newVelY] = accelSpeed > 0 ? accelerate(VARIABLES.velX, VARIABLES.velY, VectorMath.wishDirX(), VectorMath.wishdirY(), VectorMath.wishspeed(), 10, 0);
+    
+    // Use dt in accelerate(velX, velY, ..., dt)
+    requestAnimationFrame(gameLoop);
 }
