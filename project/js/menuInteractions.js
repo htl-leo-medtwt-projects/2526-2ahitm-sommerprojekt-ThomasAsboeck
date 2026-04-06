e9a883516873f90db0455e11689c8bbaee43adf3
Thomas <t.asboeck@students.htl-leonding.ac.at>
2026-04-06 22:32:10 +0200

    updated Visuals and started with the settings

--- project/js/menuInteractions.js
+++ project/js/menuInteractions.js
@@ -20,3 +20,22 @@ function enterLevelSelectFromMainMenu() {
             </div>`;
     document.getElementById("screen").innerHTML = html;   
 }
+
+function enterSettingsFromMainMenu() {
+    let html = "";
+    html = `<div id="settings-container">
+                
+                <div id="settings-volume-container" class="individuial-settings-container">
+                    <div id="settings-volume-title" class="settings-title"></div>
+                    <input type="range" min="0" max="100" step="1"value="100">
+                </div>
+
+                <div id="settings-music-container" class="individuial-settings-container">
+                </div>
+
+                <div id="settings-shaking-container" class="individuial-settings-container">
+                </div>
+
+            </div>`;
+    document.getElementById("screen").innerHTML = html;   
+}
