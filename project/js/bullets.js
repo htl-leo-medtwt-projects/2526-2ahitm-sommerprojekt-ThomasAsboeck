d6914894716b2b5099c1e6dc3b86c188f66b89fa
Thomas <t.asboeck@students.htl-leonding.ac.at>
2026-04-15 17:12:19 +0200

    removed the ability to piss onto the walls

--- project/js/bullets.js
+++ project/js/bullets.js
@@ -8,8 +8,8 @@ function shoot() {
     element.className = "bullet"
     element.id = "bullet" + counter
     let bullet = {
-        bulletX: 320 + (COORDINATES.playerX*-1),
-        bulletY: 180 + (COORDINATES.playerY*-1),
+        bulletX: 320 + (COORDINATES.playerX * -1),
+        bulletY: 180 + (COORDINATES.playerY * -1),
         velocityX: forwardX * bulletSpeed,
         velocityY: forwardY * bulletSpeed,
         damage: 25,
@@ -23,11 +23,30 @@ function shoot() {
     counter++;
 }
 
+let wasLastNull = false;
 function highQualityBulletMath() {
     for (let i = 0; i < bullets.length; i++) {
-        bullets[i].bulletX += bullets[i].velocityX * deltaTime;
-        bullets[i].bulletY -= bullets[i].velocityY * deltaTime;
-        bullets[i].element.style.left = bullets[i].bulletX + "px";
-        bullets[i].element.style.bottom = bullets[i].bulletY + "px";
+        if (bullets[i] != null) {
+            bullets[i].bulletX += bullets[i].velocityX * deltaTime;
+            bullets[i].bulletY -= bullets[i].velocityY * deltaTime;
+            bullets[i].element.style.left = bullets[i].bulletX + "px";
+            bullets[i].element.style.bottom = bullets[i].bulletY + "px";
+            bullets[i].lifetime--;
+            if (bullets[i].lifetime <= 0) {
+                bullets[i] = null;
+            }
+        }
+    }
+    checkBulletCollisions();
+}
+
+function checkBulletCollisions() {
+    for (let i = 0; i < bullets.length; i++) {
+        if (bullets[i] == null || bullets[i].bulletX < 0 || bullets[i].bulletX > 2160 - 180 || bullets[i].bulletY < 0 || bullets[i].bulletY > 3840 - 320) {
+            if (document.getElementById(`bullet${i}`)) {
+                document.getElementById(`bullet${i}`).remove();
+            }
+            bullets[i] = null;
+        }
     }
 }
