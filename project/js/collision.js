0475b39cc197c4fead5eb7c66f6a2b534b86b9cf
Thomas <t.asboeck@students.htl-leonding.ac.at>
2026-05-17 18:24:30 +0200

    Added janky enemy collisions

--- project/js/collision.js
+++ project/js/collision.js
@@ -1,18 +1,32 @@
 
 function bulletCollisionEnemy(enemy, bullet) {
-    let enemyX = enemy.enemyX + enemy.size/2;
-    let enemyY = enemy.enemyY + enemy.size/2;
-    let bulletX = bullet.bulletX + bullet.size/2;
-    let bulletY = bullet.bulletY + bullet.size/2;
+    let enemyX = enemy.enemyX + enemy.size / 2;
+    let enemyY = enemy.enemyY + enemy.size / 2;
+    let bulletX = bullet.bulletX + bullet.size / 2;
+    let bulletY = bullet.bulletY + bullet.size / 2;
     let dx = bulletX - bullet.bulletOldX;
     let dy = bulletY - bullet.bulletOldY;
 
-    t = ((enemyX - bullet.bulletOldX) * dx + (enemyY - bullet.bulletOldY) * dy) / (dx*dx + dy*dy);
+    t = ((enemyX - bullet.bulletOldX) * dx + (enemyY - bullet.bulletOldY) * dy) / (dx * dx + dy * dy);
     t = Math.max(0, Math.min(1, t));
 
     closestX = bullet.bulletOldX + t * dx
     closestY = bullet.bulletOldY + t * dy
 
     let distanceBulletToEnemy = Math.sqrt((closestX - enemyX) * (closestX - enemyX) + (closestY - enemyY) * (closestY - enemyY));
-    return (distanceBulletToEnemy < enemy.size/2);
+    return (distanceBulletToEnemy < enemy.size / 2);
 };
+
+function EnemyCollisionEnemy(enemy1, enemy2) {
+    let enemy1X = enemy1.enemyX + enemy1.size / 2;
+    let enemy1Y = enemy1.enemyY + enemy1.size / 2;
+
+    let enemy2X = enemy2.enemyX + enemy2.size / 2;
+    let enemy2Y = enemy2.enemyY + enemy2.size / 2;
+
+    let distance = Math.sqrt((enemy1X - enemy2X) * (enemy1X - enemy2X) + (enemy1Y - enemy2Y) * (enemy1Y - enemy2Y));
+
+    let overlap = (enemy1.size / 2 + enemy2.size / 2) - distance;
+
+    return (overlap > 0);
+}
--- project/js/enemys.js
+++ project/js/enemys.js
@@ -9,13 +9,17 @@ function addEnemy() {
 
     let enemy = {
         hp: 100,
-        enemyX: Math.floor(rng()* (3200)) + 304,
-        enemyY: Math.floor(rng()* (1800)) + 164,
-        enemyVelocityX: 0,
-        enemyVelocityY: 0,
+        enemyX: Math.floor(rng() * (3200)) + 304,
+        enemyY: Math.floor(rng() * (1800)) + 164,
         enemyOldX: 0,
         enemyOldY: 0,
+        speed: 0.32 * (1 + rng() * 0.1),
         size: 32,
+        enemyDirectionX: 0,
+        enemyDirectionY: 0,
+        enemySpeedX: 0,
+        enemySpeedY: 0,
+        targetLength: 0,
         element: element
     }
 
@@ -26,7 +30,7 @@ function addEnemy() {
 }
 
 function enemyLogic() {
-    if(enemysInWorld < 5 * player.difficulty) {
+    if (enemysInWorld < 5 * player.difficulty) {
         addEnemy();
     }
 
@@ -37,18 +41,111 @@ function enemyLogic() {
     }
     for (let i = 0; i < enemys.length; i++) {
         if (enemys[i] != null) {
-            enemys[i].element.style.left = enemys[i].enemyX + "px";
-            enemys[i].element.style.bottom = enemys[i].enemyY + "px";
+            let blockedX = false;
+            let blockedY = false;
+            enemys[i].enemyOldX = enemys[i].enemyX;
+            enemys[i].enemyOldY = enemys[i].enemyY;
+            enemyMovement(enemys[i]);
+            enemys[i].enemyX += enemys[i].enemySpeedX;
+            for (let j = 0; j < enemys.length; j++) {
+                if (enemys[j] != null && i != j) {
+                    if (EnemyCollisionEnemy(enemys[i], enemys[j])) {
+                        enemys[i].enemyX = enemys[i].enemyOldX;
+                        blockedX = true;
+                    }
+                }
+            }
+            enemys[i].enemyY += enemys[i].enemySpeedY;
+            for (let j = 0; j < enemys.length; j++) {
+                if (enemys[j] != null && i != j) {
+                    if (EnemyCollisionEnemy(enemys[i], enemys[j])) {
+                        enemys[i].enemyY = enemys[i].enemyOldY;
+                        blockedY = true;
+                    }
+                }
+            }
+            if (blockedX && blockedY) {
+                enemyCollisionLogic(enemys[i]);
+            }
         }
+        enemys[i].element.style.left = enemys[i].enemyX + "px";
+        enemys[i].element.style.bottom = enemys[i].enemyY + "px";
     }
-
 }
+
 
 function killEnemy(i) {
     if (document.getElementById(`enemy${i}`)) {
         enemys[i] = null;
         document.getElementById(`enemy${i}`).remove();
         enemysInWorld--;
-        player.coins += multipliers.coins * Math.ceil(rng()*3);
+        player.coins += multipliers.coins * Math.ceil(rng() * 3);
+    }
+}
+
+function enemyMovement(enemy) {
+    enemy.enemySpeedX = 0;
+    enemy.enemySpeedY = 0;
+
+    let playerX = player.playerX * -1 + 304;
+    let playerY = player.playerY * -1 + 164;
+
+    enemy.enemyDirectionX = playerX - enemy.enemyX;
+    enemy.enemyDirectionY = playerY - enemy.enemyY;
+
+    let length = Math.sqrt(enemy.enemyDirectionX * enemy.enemyDirectionX + enemy.enemyDirectionY * enemy.enemyDirectionY);
+
+    if (length != 0) {
+        enemy.enemyDirectionX = enemy.enemyDirectionX / length;
+        enemy.enemyDirectionY = enemy.enemyDirectionY / length;
+    }
+
+    let targetX = playerX - enemy.enemyDirectionX * 32;
+    let targetY = playerY - enemy.enemyDirectionY * 32;
+
+    enemy.enemyDirectionX = targetX - enemy.enemyX;
+    enemy.enemyDirectionY = targetY - enemy.enemyY;
+
+    enemy.targetLength = Math.sqrt(enemy.enemyDirectionX * enemy.enemyDirectionX + enemy.enemyDirectionY * enemy.enemyDirectionY);
+
+    if (enemy.targetLength != 0) {
+        enemy.enemyDirectionX = enemy.enemyDirectionX / enemy.targetLength;
+        enemy.enemyDirectionY = enemy.enemyDirectionY / enemy.targetLength;
+    }
+    if (enemy.targetLength > 3) {
+        enemy.enemySpeedX = enemy.enemyDirectionX * deltaTime * enemy.speed;
+        enemy.enemySpeedY = enemy.enemyDirectionY * deltaTime * enemy.speed;
+    }
+}
+
+function enemyCollisionLogic(enemy) {
+    if (enemy.targetLength > 3) {
+        let playerX = player.playerX * -1 + 304;
+        let playerY = player.playerY * -1 + 164;
+
+        let targetX = playerX - enemy.enemyDirectionX * 32;
+        let targetY = playerY - enemy.enemyDirectionY * 32;
+
+        let leftX = -enemy.enemyDirectionY;
+        let leftY = enemy.enemyDirectionX;
+        let rightX = enemy.enemyDirectionY;
+        let rightY = -enemy.enemyDirectionX;
+
+        let leftPosX = enemy.enemyX + leftX;
+        let leftPosY = enemy.enemyY + leftY;
+        let rightPosX = enemy.enemyX + rightX;
+        let rightPosY = enemy.enemyY + rightY;
+
+        let leftDistance = Math.sqrt((leftPosX - targetX) * (leftPosX - targetX) + (leftPosY - targetY) * (leftPosY - targetY));
+        let rightDistance = Math.sqrt((rightPosX - targetX) * (rightPosX - targetX) + (rightPosY - targetY) * (rightPosY - targetY));
+
+        if (leftDistance < rightDistance) {
+            enemy.enemyX += leftX * enemy.speed * deltaTime * (enemy.speed / 2);
+            enemy.enemyY += leftY * enemy.speed * deltaTime * (enemy.speed / 2);
+        }
+        else {
+            enemy.enemyX += rightX * enemy.speed * deltaTime * (enemy.speed / 2);
+            enemy.enemyY += rightY * enemy.speed * deltaTime * (enemy.speed / 2);
+        }
     }
 }
