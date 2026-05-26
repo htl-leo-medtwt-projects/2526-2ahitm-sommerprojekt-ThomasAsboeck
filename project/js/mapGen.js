848e83743bc68109214589e1ef73f26e598c13da
Thomas <t.asboeck@students.htl-leonding.ac.at>
2026-05-26 14:45:06 +0200

    continued on the map algorithm

--- project/js/mapGen.js
+++ project/js/mapGen.js
@@ -12,16 +12,51 @@ function initMap() {
     }
 }
 
-placeRoad(startX, startY, direction, length) {
+function placeRoad(startX, startY, direction, length) {
     if (direction == "horizontal") {
-        for (let i = 0; i < length; i++) {
-            
-        }
-        for (let j = 0; j < ROAD_WIDTH; j++) {
-            map[startY + i][startX] = 1
+        if (startX + length <= MAP_WIDTH && startY + ROAD_WIDTH <= MAP_HEIGHT) {
+            for (let i = 0; i < length; i++) {
+                for (let j = 0; j < ROAD_WIDTH; j++) {
+                    map[startY + j][startX + i] = 1;
+                }
+            }
         }
     }
     else if (direction == "vertical") {
-
+        if (startY + length <= MAP_HEIGHT && startX + ROAD_WIDTH <= MAP_WIDTH) {
+            for (let i = 0; i < length; i++) {
+                for (let j = 0; j < ROAD_WIDTH; j++) {
+                    map[startY + i][startX + j] = 1;
+                }
+            }
+        }
     }
 }
+
+function generateRoadNetwork() {
+    placeRoad(64, 0, "vertical", MAP_HEIGHT);
+    placeRoad(0, 64, "horizontal", MAP_WIDTH);
+}
+
+function generateBranchHorizontally(startX, startY) {
+    let randomLength = Math.floor(rng()*(MAP_WIDTH-startX));
+    placeRoad(startX, startY, "horizontal", randomLength);
+}
+
+function generateBranches() {
+    for (let y = )
+}
+
+function debugMap() {
+    let output = "";
+    for (let y = 0; y < MAP_HEIGHT; y++) {
+        for (let x = 0; x < MAP_WIDTH; x++) {
+            output += map[y][x] + " ";
+        }
+        output += "\n";
+    }
+    console.log(output);
+}
+
+initMap();
+generateRoadNetwork();
